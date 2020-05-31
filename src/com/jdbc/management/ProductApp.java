3fe2bcc1ad45150039aae65ab57e0d763ab776cc
Miguel Feo <miguel.feo@vmlyr.com>
2020-05-30 19:29:01 -0500

    First commit

---
+++ src/com/jdbc/management/ProductApp.java
@@ -0,0 +1,15 @@
+package com.jdbc.management;
+
+import com.jdbc.data.ProductJDBC;
+
+public class ProductApp {
+    public static void main(String[] args) {
+
+        ProductJDBC productJDBC = new ProductJDBC();
+
+        //productJDBC.insert("Phone", 12000, 3);
+        //productJDBC.update(3, "PhoneChanged", 45000, 3);
+        //productJDBC.select();
+
+    }
+}
